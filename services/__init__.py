1c0db1bdd41e5b788b14ea73feb5925d73c3b25e
Administrator <admin@example.com>
2023-11-30 14:21:50 +0800

    change config

--- services/__init__.py
+++ services/__init__.py
@@ -1,13 +1,17 @@
 # Establish a connection
 
 import psycopg2
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+import os
 
 
 def create_connection():
     return psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="postgres",
-        user="postgres",
-        password="admin",
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
     )
